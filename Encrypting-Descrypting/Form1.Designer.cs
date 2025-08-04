a30b78b7b869ff2c5c89631095c9bca2fb1f7fe9
Boulay Soumia <Worksoumia@gmail.com>
2025-08-04 20:00:39 +0200

    Add files via upload

---
+++ Encrypting-Descrypting/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Projet_Encrypting_Descrepting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEncrypting = new Guna.UI2.WinForms.Guna2TextBox();
+            this.txtDecrypting = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.txtEncrypting1 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.txtDecrypting1 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2ImageButton2 = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.guna2ImageButton1 = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.SuspendLayout();
+            // 
+            // txtEncrypting
+            // 
+            this.txtEncrypting.Animated = true;
+            this.txtEncrypting.AutoRoundedCorners = true;
+            this.txtEncrypting.BackColor = System.Drawing.Color.Aqua;
+            this.txtEncrypting.BorderRadius = 63;
+            this.txtEncrypting.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtEncrypting.DefaultText = "";
+            this.txtEncrypting.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtEncrypting.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtEncrypting.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtEncrypting.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtEncrypting.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtEncrypting.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEncrypting.ForeColor = System.Drawing.Color.Black;
+            this.txtEncrypting.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtEncrypting.Location = new System.Drawing.Point(31, 411);
+            this.txtEncrypting.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtEncrypting.Multiline = true;
+            this.txtEncrypting.Name = "txtEncrypting";
+            this.txtEncrypting.PasswordChar = '\0';
+            this.txtEncrypting.PlaceholderText = "";
+            this.txtEncrypting.SelectedText = "";
+            this.txtEncrypting.Size = new System.Drawing.Size(348, 128);
+            this.txtEncrypting.TabIndex = 1;
+            // 
+            // txtDecrypting
+            // 
+            this.txtDecrypting.Animated = true;
+            this.txtDecrypting.AutoRoundedCorners = true;
+            this.txtDecrypting.BackColor = System.Drawing.Color.Aqua;
+            this.txtDecrypting.BorderRadius = 63;
+            this.txtDecrypting.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtDecrypting.DefaultText = "";
+            this.txtDecrypting.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtDecrypting.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtDecrypting.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtDecrypting.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtDecrypting.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtDecrypting.Font = new System.Drawing.Font("Segoe UI", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDecrypting.ForeColor = System.Drawing.Color.Black;
+            this.txtDecrypting.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtDecrypting.Location = new System.Drawing.Point(445, 411);
+            this.txtDecrypting.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtDecrypting.Multiline = true;
+            this.txtDecrypting.Name = "txtDecrypting";
+            this.txtDecrypting.PasswordChar = '\0';
+            this.txtDecrypting.PlaceholderText = "";
+            this.txtDecrypting.SelectedText = "";
+            this.txtDecrypting.Size = new System.Drawing.Size(342, 128);
+            this.txtDecrypting.TabIndex = 1;
+            // 
+            // guna2HtmlLabel2
+            // 
+            this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel2.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel2.Location = new System.Drawing.Point(526, 44);
+            this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            this.guna2HtmlLabel2.Size = new System.Drawing.Size(3, 2);
+            this.guna2HtmlLabel2.TabIndex = 3;
+            this.guna2HtmlLabel2.Text = null;
+            // 
+            // txtEncrypting1
+            // 
+            this.txtEncrypting1.Animated = true;
+            this.txtEncrypting1.AutoRoundedCorners = true;
+            this.txtEncrypting1.BackColor = System.Drawing.Color.Aqua;
+            this.txtEncrypting1.BorderRadius = 81;
+            this.txtEncrypting1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtEncrypting1.DefaultText = "";
+            this.txtEncrypting1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtEncrypting1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtEncrypting1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtEncrypting1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtEncrypting1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtEncrypting1.Font = new System.Drawing.Font("Segoe UI", 10.2F);
+            this.txtEncrypting1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtEncrypting1.Location = new System.Drawing.Point(44, 56);
+            this.txtEncrypting1.Margin = new System.Windows.Forms.Padding(4, 7, 4, 7);
+            this.txtEncrypting1.Multiline = true;
+            this.txtEncrypting1.Name = "txtEncrypting1";
+            this.txtEncrypting1.PasswordChar = '\0';
+            this.txtEncrypting1.PlaceholderText = "";
+            this.txtEncrypting1.SelectedText = "";
+            this.txtEncrypting1.Size = new System.Drawing.Size(323, 165);
+            this.txtEncrypting1.TabIndex = 0;
+            // 
+            // txtDecrypting1
+            // 
+            this.txtDecrypting1.Animated = true;
+            this.txtDecrypting1.AutoRoundedCorners = true;
+            this.txtDecrypting1.BackColor = System.Drawing.Color.Aqua;
+            this.txtDecrypting1.BorderRadius = 78;
+            this.txtDecrypting1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtDecrypting1.DefaultText = "";
+            this.txtDecrypting1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtDecrypting1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtDecrypting1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtDecrypting1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtDecrypting1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtDecrypting1.Font = new System.Drawing.Font("Segoe UI", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDecrypting1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtDecrypting1.Location = new System.Drawing.Point(434, 56);
+            this.txtDecrypting1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtDecrypting1.Multiline = true;
+            this.txtDecrypting1.Name = "txtDecrypting1";
+            this.txtDecrypting1.PasswordChar = '\0';
+            this.txtDecrypting1.PlaceholderText = "";
+            this.txtDecrypting1.SelectedText = "";
+            this.txtDecrypting1.Size = new System.Drawing.Size(337, 159);
+            this.txtDecrypting1.TabIndex = 7;
+            // 
+            // guna2ImageButton2
+            // 
+            this.guna2ImageButton2.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.guna2ImageButton2.HoverState.ImageSize = new System.Drawing.Size(80, 80);
+            this.guna2ImageButton2.Image = global::Projet_Encrypting_Descrepting.Properties.Resources.encryption;
+            this.guna2ImageButton2.ImageOffset = new System.Drawing.Point(0, 0);
+            this.guna2ImageButton2.ImageRotate = 0F;
+            this.guna2ImageButton2.ImageSize = new System.Drawing.Size(150, 150);
+            this.guna2ImageButton2.Location = new System.Drawing.Point(109, 237);
+            this.guna2ImageButton2.Name = "guna2ImageButton2";
+            this.guna2ImageButton2.PressedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.guna2ImageButton2.Size = new System.Drawing.Size(194, 166);
+            this.guna2ImageButton2.TabIndex = 9;
+            this.guna2ImageButton2.Click += new System.EventHandler(this.guna2ImageButton2_Click);
+            // 
+            // guna2ImageButton1
+            // 
+            this.guna2ImageButton1.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.guna2ImageButton1.HoverState.ImageFlip = Guna.UI2.WinForms.Enums.FlipOrientation.Horizontal;
+            this.guna2ImageButton1.HoverState.ImageSize = new System.Drawing.Size(70, 70);
+            this.guna2ImageButton1.Image = global::Projet_Encrypting_Descrepting.Properties.Resources.decrypt;
+            this.guna2ImageButton1.ImageOffset = new System.Drawing.Point(0, 0);
+            this.guna2ImageButton1.ImageRotate = 0F;
+            this.guna2ImageButton1.ImageSize = new System.Drawing.Size(130, 130);
+            this.guna2ImageButton1.Location = new System.Drawing.Point(535, 237);
+            this.guna2ImageButton1.Name = "guna2ImageButton1";
+            this.guna2ImageButton1.PressedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.guna2ImageButton1.Size = new System.Drawing.Size(170, 151);
+            this.guna2ImageButton1.TabIndex = 8;
+            this.guna2ImageButton1.Click += new System.EventHandler(this.guna2ImageButton1_Click);
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel1.ForeColor = System.Drawing.Color.White;
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(95, 13);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(185, 33);
+            this.guna2HtmlLabel1.TabIndex = 10;
+            this.guna2HtmlLabel1.Text = "Encrypting Text";
+            // 
+            // guna2HtmlLabel3
+            // 
+            this.guna2HtmlLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel3.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel3.ForeColor = System.Drawing.Color.White;
+            this.guna2HtmlLabel3.Location = new System.Drawing.Point(503, 13);
+            this.guna2HtmlLabel3.Name = "guna2HtmlLabel3";
+            this.guna2HtmlLabel3.Size = new System.Drawing.Size(202, 33);
+            this.guna2HtmlLabel3.TabIndex = 11;
+            this.guna2HtmlLabel3.Text = "Decrypting Text   .";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
+            this.ClientSize = new System.Drawing.Size(800, 571);
+            this.Controls.Add(this.guna2HtmlLabel3);
+            this.Controls.Add(this.guna2HtmlLabel1);
+            this.Controls.Add(this.guna2ImageButton2);
+            this.Controls.Add(this.guna2ImageButton1);
+            this.Controls.Add(this.txtDecrypting1);
+            this.Controls.Add(this.txtEncrypting1);
+            this.Controls.Add(this.guna2HtmlLabel2);
+            this.Controls.Add(this.txtDecrypting);
+            this.Controls.Add(this.txtEncrypting);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2TextBox txtEncrypting;
+        private Guna.UI2.WinForms.Guna2TextBox txtDecrypting;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
+        private Guna.UI2.WinForms.Guna2TextBox txtEncrypting1;
+        private Guna.UI2.WinForms.Guna2TextBox txtDecrypting1;
+        private Guna.UI2.WinForms.Guna2ImageButton guna2ImageButton1;
+        private Guna.UI2.WinForms.Guna2ImageButton guna2ImageButton2;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel3;
+    }
+}
+
---
+++ Encrypting-Descrypting/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projet_Encrypting_Descrepting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+
+       
+        void Encryption(string Text, int key )
+        {
+
+            for (int i = 0; i <Text.Length; i++)
+            {
+                char c = Text[i] ;
+
+                c = (char)(c +i + key);
+
+
+                txtEncrypting.Text += c;
+
+                
+
+
+            }
+
+
+        }
+
+
+        void Decryption(string Text, int key)
+        {
+
+            for (int i = 0; i < Text.Length; i++)
+            {
+                char c = Text[i];
+
+                c = (char)(c -i   - key);
+
+
+                txtDecrypting.Text += c;
+
+
+
+
+            }
+
+
+        }
+        private void guna2CircleButton1_Click(object sender, EventArgs e)
+        {
+
+           
+
+           
+
+
+
+        }
+
+        private void guna2CircleButton2_Click(object sender, EventArgs e)
+        {
+
+           
+        }
+
+        private void guna2ImageButton1_Click(object sender, EventArgs e)
+        {
+            Decryption(txtEncrypting.Text, 2);
+            txtEncrypting1.Clear();
+        }
+
+        private void guna2ImageButton2_Click(object sender, EventArgs e)
+        {
+            txtDecrypting.Clear();
+            txtDecrypting1.Clear();
+            txtEncrypting.Clear();
+
+
+            Encryption(txtEncrypting1.Text, 2);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtEncrypting1.Focus();
+
+        }
+    }
+}
